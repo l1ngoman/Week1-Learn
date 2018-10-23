780bb1386f217e29550cdb8cc3f7b75793e95605
l1ngoman <41375558+l1ngoman@users.noreply.github.com>
2018-10-23 09:51:13 -0700

    challenge 10-23

--- codeDrills.js
+++ codeDrills.js
@@ -1,16 +1,60 @@
 //************CODE DRILLS **********************
 
-var numArr = [3,5,16,18,9,11,26];
+// var numArr = [3,5,16,18,9,11,26];
+//
+// function outputOdds(arr){
+// var newArr = [];
+//
+//   for(var i = 0; i<arr.length; i++){
+//     if(arr[i]%2 != 0){
+//       newArr.push(arr[i]);
+//     }
+//   }
+//   console.log(newArr);
+// }
+//
+// outputOdds(numArr);
 
-function outputOdds(arr){
-var newArr = [];
+// var numArr = [8,7,5,4,3,1,7,6];
+// let newArr = [];
+// for(let i=0;i<numArr.length;i++){
+//  newArr.push(numArr[i]*3);
+// }
+// console.log(newArr);
+//
+// var byThree = (arr) => {
+//   let newArr2 = [];
+//   for (let i=0; i<arr.length;i++) {
+//     newArr2.push(arr[i]*3)
+//   }
+//   return newArr2;
+// }
+//
+// console.log(byThree([1,2,3,4]));
+//
+// var useMap = (arr) => {
+//   return  arr.map((el) => el*3)
+//
+// }
+// console.log(useMap([1,2,3,4,5]))
 
-  for(var i = 0; i<arr.length; i++){
-    if(arr[i]%2 != 0){
-      newArr.push(arr[i]);
-    }
+var arr = ["y",5,7,"ten",2,15,"z"]
+
+var getNums = (arr) => {
+  newArr = []
+  for(let i=0;i<arr.length;i++){
+      if(typeof arr[i]=="number"){
+        newArr.push(arr[i])
+      }
   }
-  console.log(newArr);
+  return newArr;
 }
 
-outputOdds(numArr);
+console.log(getNums(arr))
+
+
+var getNumsWithFilter = (arr) => {
+  return arr.filter(el => (typeof el == "number")) 
+}
+
+console.log(getNumsWithFilter(arr));
